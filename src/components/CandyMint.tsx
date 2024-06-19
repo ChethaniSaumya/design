16dbc702a95080653a70148a37cb20c0d0bf28a4
Chethani Saumya <105449384+ChethaniSaumya@users.noreply.github.com>
2024-06-19 08:38:25 +0530

    Add files via upload

---
+++ src/components/CandyMint.tsx
@@ -0,0 +1,110 @@
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { LAMPORTS_PER_SOL, TransactionSignature } from '@solana/web3.js';
+import { FC, useCallback, useMemo, useState } from 'react';
+import { notify } from "../utils/notifications";
+import useUserSOLBalanceStore from '../stores/useUserSOLBalanceStore';
+
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import { generateSigner, transactionBuilder, publicKey, some } from '@metaplex-foundation/umi';
+import { fetchCandyMachine, mintV2, mplCandyMachine, safeFetchCandyGuard } from "@metaplex-foundation/mpl-candy-machine";
+import { walletAdapterIdentity } from '@metaplex-foundation/umi-signer-wallet-adapters';
+import { mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata';
+import { setComputeUnitLimit } from '@metaplex-foundation/mpl-toolbox';
+import { clusterApiUrl } from '@solana/web3.js';
+import * as bs58 from 'bs58';
+
+const quicknodeEndpoint = process.env.NEXT_PUBLIC_RPC || clusterApiUrl('devnet');
+const candyMachineAddress = publicKey(process.env.NEXT_PUBLIC_CANDY_MACHINE_ID);
+const treasury = publicKey(process.env.NEXT_PUBLIC_TREASURY);
+
+export const CandyMint: FC = () => {
+    const { connection } = useConnection();
+    const wallet = useWallet();
+    const { getUserSOLBalance } = useUserSOLBalanceStore();
+    const[_itemsRedeemed, setItemsRedeemed] = useState(0);
+
+    const umi = useMemo(() =>
+        createUmi(quicknodeEndpoint)
+            .use(walletAdapterIdentity(wallet))
+            .use(mplCandyMachine())
+            .use(mplTokenMetadata()),
+        [wallet, mplCandyMachine, walletAdapterIdentity, mplTokenMetadata, quicknodeEndpoint, createUmi]
+    );
+
+    const onClick = useCallback(async () => {
+        if (!wallet.publicKey) {
+            console.log('error', 'Wallet not connected!');
+            notify({ type: 'error', message: 'error', description: 'Wallet not connected!' });
+            return;
+        }
+
+        // Fetch the Candy Machine.
+        const candyMachine = await fetchCandyMachine(
+            umi,
+            candyMachineAddress,
+        );
+        // Fetch the Candy Guard.
+        const candyGuard = await safeFetchCandyGuard(
+            umi,
+            candyMachine.mintAuthority,
+        );
+
+        // Fetch the Candy Guard.
+        const itemsRedeemed = candyMachine.itemsRedeemed;
+        console.log("itemsRedeemed :" + itemsRedeemed);
+        setItemsRedeemed(Number(itemsRedeemed));
+
+        try {
+            // Mint from the Candy Machine.
+            const nftMint = generateSigner(umi);
+            const transaction = await transactionBuilder()
+                .add(setComputeUnitLimit(umi, { units: 800_000 }))
+                .add(
+                    mintV2(umi, {
+                        candyMachine: candyMachine.publicKey,
+                        candyGuard: candyGuard?.publicKey,
+                        nftMint,
+                        collectionMint: candyMachine.collectionMint,
+                        collectionUpdateAuthority: candyMachine.authority,
+                        mintArgs: {
+                            solPayment: some({ destination: treasury }),
+                        },
+                    })
+                );
+            const { signature } = await transaction.sendAndConfirm(umi, {
+                confirm: { commitment: "confirmed" },
+            });
+            const txid = bs58.encode(signature);
+            console.log('success', `Mint successful! ${txid}`)
+            notify({ type: 'success', message: 'Mint successful!', txid });
+
+            getUserSOLBalance(wallet.publicKey, connection);
+        } catch (error: any) {
+            notify({ type: 'error', message: `Error minting!`, description: error?.message });
+            console.log('error', `Mint failed! ${error?.message}`);
+        }
+    }, [wallet, connection, getUserSOLBalance, umi, candyMachineAddress, treasury]);
+
+    return (
+
+        <div className="flex flex-row justify-center mt-10 ">
+            <div className="relative group items-center">
+                <div className="m-1 absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-fuchsia-500 
+                    rounded-lg blur opacity-20 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+
+                <button id="rBtn"
+                    className="text-3xl px-7 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
+                    onClick={onClick}
+                >
+                    <span id="mintNFT">Mint NFT</span>
+                </button>
+
+               {/* <div>{_itemsRedeemed} Minted</div>*/}
+
+            </div>
+        </div>
+
+
+    );
+};
+
